4ba438457c27066768f8d4d66a6563fbc5ea0e90
Sio <nyakya393@gmail.com>
2026-07-06 19:39:39 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+// ======================================
+// App
+// ======================================
 
+window.addEventListener("load",()=>{
+
+    const loading = document.getElementById("loadingScreen");
+
+    setTimeout(()=>{
+
+        if(loading){
+
+            loading.style.opacity="0";
+
+            setTimeout(()=>{
+
+                loading.remove();
+
+            },400);
+
+        }
+
+    },500);
+
+    showPage("home");
+
+});
