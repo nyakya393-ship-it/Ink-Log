6dc87f4a30e8d39725ff688a95a9c69c1696d690
Sio <nyakya393@gmail.com>
2026-07-06 20:13:25 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 // ======================================
-// App
+// InkLog Ultimate
+// app.js
 // ======================================
 
-window.addEventListener("load",()=>{
+window.addEventListener("load", async()=>{
 
-　　　await Storage.openDatabase();
-    
-    const loading = document.getElementById("loadingScreen");
+    try{
 
-    setTimeout(()=>{
+        // IndexedDB
+        await Storage.openDatabase();
+
+        // 最初のページ
+        showPage("home");
+
+        // ローディング終了
+        const loading=document.getElementById("loadingScreen");
 
         if(loading){
 
@@ -18,20 +24,37 @@ window.addEventListener("load",()=>{
 
                 loading.remove();
 
-            },400);
+            },300);
 
         }
 
-    },500);
+        // 起動メッセージ
+        UI.showToast("InkLog Ultimate 起動完了");
 
-    showPage("home");
+    }catch(error){
 
-    UI.showToast("InkLogへようこそ！");
+        console.error(error);
+
+        alert("起動中にエラーが発生しました。");
+
+    }
+
+    // Service Worker
+
+    if("serviceWorker" in navigator){
+
+        try{
+
+            await navigator.serviceWorker.register("./sw.js");
+
+            console.log("Service Worker OK");
+
+        }catch(e){
+
+            console.error(e);
+
+        }
+
+    }
 
 });
-
-if("serviceWorker" in navigator){
-
-    navigator.serviceWorker.register("sw.js");
-
-}
